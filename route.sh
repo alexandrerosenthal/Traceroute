9e2b20c49b7fdb205dd3c6c379966c670c26b997
test <test@213-99.iutbeziers.fr>
2019-10-04 11:31:04 +0200

    Première essaie script symbole -I innatendu ligne 7

--- route.sh
+++ route.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 echo 'Entrez le nom de fichier de sorti :'
 read fichier
+echo 'Entrez une IP ou un nom de domaine :'
+read ip
 mttl=1
-protocole(-I -T)
-commande=$(traceroute -n -A $protocole -m $mttl )
+protocole('-I' '-T')
+commande=$(traceroute -n -A $protocole -m $mttl $ip)
 for i in $protocole
 do
 	for $mttl in $(seq 1 10)
